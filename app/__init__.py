82077abf55c29103ffbf092380d898efbaf50e1c
Jen <85088293+jen-tang@users.noreply.github.com>
2025-07-30 18:34:26 -0400

    Revert "Create database and flask tests"

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,7 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv()
 app = Flask(__name__)
 
-if os.getenv('TESTING') == 'true':
-    print('Running in testing mode')
-    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
-else:
-    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
               user=os.getenv("MYSQL_USER"),
               password=os.getenv("MYSQL_PASSWORD"),
               host=os.getenv("MYSQL_HOST"),
@@ -37,16 +33,9 @@ mydb.create_tables([TimelinePost])
 
 @app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
-    try:
-        name = request.form["name"]
-    except KeyError:
-        return 'Invalid name', 400
+    name = request.form["name"]
     email = request.form["email"]
-    if '@' not in email:
-        return 'Invalid email', 400
     content = request.form["content"]
-    if not content:
-        return 'Invalid content', 400
     timeline_post = TimelinePost.create(
         name=name,
         email=email,
